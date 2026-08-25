955e446b5c1cdf05de1aeac87258a4339b942020
IanCity <iwciu990@learner.dvusd.org>
2026-08-25 07:49:17 -0700

    First

---
+++ sharp/Kiosk/Program.cs
@@ -0,0 +1,21 @@
+﻿// BCH Software Inc. - Sprint 1: Interactive Kiosk
+// Track: C# / CIS151 Software Engineering
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BCHSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("========================================");
+            Console.WriteLine("      BCH ENTERPRISE VISITOR KIOSK      ");
+            Console.WriteLine("========================================");
+
+            // SE: Use Console.ReadLine() to capture 4 data fields
+            // SE: Use Console.WriteLine() to render the ASCII badge
+        }
+    }
+}
